95bd1e56d18693328713b7c51e07d5ee6d093017
sandroroux <aroux@edx.org>
2018-04-18 15:17:34 -0400

    Adds retire_user mixin.

--- openedx/core/djangolib/model_mixins.py
+++ openedx/core/djangolib/model_mixins.py
@@ -12,3 +12,25 @@ class DeprecatedModelMixin(object):
         Override to kill usage of this model.
         """
         raise TypeError("This model has been deprecated and should not be used.")
+
+
+class DeletableByUserEmail(object):
+    """
+    This mixin allows inheriting models to delete users by their email field.
+    """
+
+    @classmethod
+    def retire_user(cls, user_email):
+        """
+        Deletes user_email's record in the cls model.
+
+        Returns True if user_email's records were deleted.
+        Returns False otherwise.
+        """
+        assert hasattr(cls, 'email'), 'Inheriting model must have an "email" field.'
+
+        user_search_results = cls.objects.filter(
+            email=user_email
+        )
+        num_deleted_records, _ = user_search_results.delete()
+        return num_deleted_records > 0
