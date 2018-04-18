1d5f237aded74e4723db8526f11f8b61b0e8f831
AlasdairSwan <aswan@edx.org>
2018-04-18 07:44:25 -0400

    Style updates

--- lms/static/js/student_account/components/StudentAccountDeletionModal.jsx
+++ lms/static/js/student_account/components/StudentAccountDeletionModal.jsx
@@ -2,7 +2,7 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Button, Modal, InputText, StatusAlert } from '@edx/paragon/static';
+import { Button, Modal, Icon, InputText, StatusAlert } from '@edx/paragon/static';
 import StringUtils from 'edx-ui-toolkit/js/utils/string-utils';
 
 class StudentAccountDeletionConfirmationModal extends React.Component {
@@ -63,12 +63,16 @@ class StudentAccountDeletionConfirmationModal extends React.Component {
         body={(
           <div>
             <StatusAlert
-              alertType="info"
               dialog={(
-                <div>
-                  <h2>{ gettext('You have selected “Delete my account.” Deletion of your account and personal data is permanent and cannot be undone. EdX will not be able to recover your account or the data that is deleted.') }</h2>
-                  <p>{ gettext('If you proceed, you will be unable to use this account to take courses on the edX app, edx.org, or any other site hosted by edX. This includes access to edx.org from your employer’s or university’s system and access to private sites offered by MIT Open Learning, Wharton Online, and Harvard Medical School.') }</p>
-                  <p dangerouslySetInnerHTML={{ __html: loseAccessText }} />
+                <div className="modal-alert">
+                  <div className="icon-wrapper">
+                    <Icon className={['fa', 'fa-exclamation-triangle']} />
+                  </div>
+                  <div className="alert-content">
+                    <h2>{ gettext('You have selected “Delete my account.” Deletion of your account and personal data is permanent and cannot be undone. EdX will not be able to recover your account or the data that is deleted.') }</h2>
+                    <p>{ gettext('If you proceed, you will be unable to use this account to take courses on the edX app, edx.org, or any other site hosted by edX. This includes access to edx.org from your employer’s or university’s system and access to private sites offered by MIT Open Learning, Wharton Online, and Harvard Medical School.') }</p>
+                    <p dangerouslySetInnerHTML={{ __html: loseAccessText }} />
+                  </div>
                 </div>
               )}
               dismissible={false}
